7a503d8b17c8dcabeec6bc73dd7e52d49b0073bb
nightlifeav1 <nightlifeav18@gmail.com>
2024-10-01 02:49:33 +0530

    Add files via upload

---
+++ rmm.ps1
@@ -0,0 +1,29 @@
+﻿# Check and Start RMM.Agent Service
+$service = Get-Service -Name 'RMM.Agent.exe' -ErrorAction SilentlyContinue
+if ($service) {
+    # Change the startup mode to Automatic
+    $service | Set-Service -StartupType Automatic  
+    # Check if the service is running
+    $serviceStatus = Get-Service -Name 'RMM.Agent.exe'
+    if ($serviceStatus.Status -eq "Running") {
+        Write-Host "Service RMM.Agent is already running."
+    } else {
+        Write-Host "Service RMM.Agent is not running. Starting now..."
+        Start-Service -Name 'RMM.Agent.exe'
+    }
+}
+
+# Check and Start Connect Service
+$service = Get-Service -Name 'Connect Service' -ErrorAction SilentlyContinue
+if ($service) {
+    # Change the startup mode to Automatic
+    $service | Set-Service -StartupType Automatic  
+    # Check if the service is running
+    $serviceStatus = Get-Service -Name 'Connect Service'
+    if ($serviceStatus.Status -eq "Running") {
+        Write-Host "Service Connect Service is already running."
+    } else {
+        Write-Host "Service Connect Service is not running. Starting now..."
+        Start-Service -Name 'Connect Service'
+    }
+}
